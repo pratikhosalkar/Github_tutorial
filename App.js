cb75d15cfc53206ed93b91c4178eadb8e47d88c6
13581415 <13581415@jewels.com>
2022-05-05 17:42:43 +0530

    Subtitles

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const App= ()=>{
     <View style={styles.container}>
         <View style={styles.carContainer}>
             <Text style={styles.titles}>MODEL S</Text>
+            <Text style={styles.subTitle}> Starting at 49L</Text>
 
         </View>
 
