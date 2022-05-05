03940334674068f14d38868c2938eeed2840a038
13581415 <13581415@jewels.com>
2022-05-05 17:10:56 +0530

    App.js is created

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text,View,Stylesheet } from "react-native";
+
+const App= ()=>{
+    <View style={styles.container}>
+        <View style={styles.carContainer}>
+            <Text style={styles.titles}>MODEL S</Text>
+
+        </View>
+
+    </View>
+}
